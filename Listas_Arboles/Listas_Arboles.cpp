cb66a78671f512a53cfb06b43281c4742d08d157
Fabian Ugalde <fugar09@gmail.com>
2015-06-03 11:38:44 -0600

    Laboratorio Terminado

--- Listas_Arboles/Listas_Arboles.cpp
+++ Listas_Arboles/Listas_Arboles.cpp
@@ -17,23 +17,38 @@ int _tmain(int argc, _TCHAR* argv[])
 	srand(time(NULL));
 
 	Lista l;
-
+	cout << "Crear una lista l con los valores, 2,3,6,7" << endl;
 	l.insertarFinal(new ElementoInt(2));
 	l.insertarFinal(new ElementoInt(3));
 	l.insertarFinal(new ElementoInt(6));
 	l.insertarFinal(new ElementoInt(7));
+	cout << "La lista l es: " << l << endl;
+	cout << endl;
+	
+	cout << "Insertar el elemento 12, al inicio" << endl;
 	l.insertarInicio(new ElementoInt(12));
+	cout <<"La lista l es: "<< l << endl;
+	cout << endl;
+
+	cout << "Insertar el elemento 67, en la posicion 2" << endl;
 	l.insertar(new ElementoInt(67), 2);
-	cout << l << endl;
+	cout << "La lista l es: " << l << endl;
+	cout << endl;
 
+	cout << "Remover el elemento del final" << endl;
 	l.removerFinal();
-	cout << l << endl;
+	cout << "La lista l es: " << l << endl;
+	cout << endl;
 
+	cout << "Remover el elemento del inicio" << endl;
 	l.removerInicio();
-	cout << l << endl;
+	cout << "La lista l es: " << l << endl;
+	cout << endl;
 
+	cout << "Remover el elemento de la posicion 2" << endl;
 	l.remover(2);
-	cout << l << endl;
+	cout << "La lista l es: " << l << endl;
+	cout << endl;
 
 	system("pause");
 	return 0;
